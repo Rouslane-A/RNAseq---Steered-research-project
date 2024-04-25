750aa644d1ec1bf1877a77e6e8ff71759cb7bca9
BismahGhafoor <148211580+BismahGhafoor@users.noreply.github.com>
2024-04-25 18:35:31 +0100

    Update HiSat2_index.sh

--- Novel Analysis/Alignment/HiSat2_index.sh
+++ Novel Analysis/Alignment/HiSat2_index.sh
@@ -1,3 +1,7 @@
+# This script builds an HISAT2 index for the human genome (hg38) using the HISAT2 aligner.
+# The script assumes the human genome FASTA file (hg38.fa) is available and produces a set of index files for efficient alignment.
+# Last modified: 25.04.2024
+
 #!/bin/bash
 
 # Load the HiSat2 module
